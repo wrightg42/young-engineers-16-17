6c33dcf8c05f7a6c67c5324d38006821cf70cbe0
wrightg42 <it42@hotmail.co.uk>
2017-02-06 23:57:48 +0000

    Change pins to match the wiring of the bot

--- Arduino/Radio.ino
+++ Arduino/Radio.ino
@@ -6,44 +6,16 @@ RCSwitchB lswitch = RCSwitchB();
 
 void RadioSetup() {
   DEBUG_PRINTLN("Begin Radio Setup");
-  rswitch.enableReceive(2);
-  lswitch.enableReceive(3);
+  lswitch.enableReceive(2);
+  rswitch.enableReceive(3);
   DEBUG_PRINTLN("End Radio Setup");
 }
-
-int GetSignalRight() {
-  if (rswitch.available()) {
-    int value = rswitch.getReceivedValue();
-    int protocol = rswitch.getReceivedProtocol();
-    int rssi = analogRead(0);
-    if (value == 47869 && protocol == 5) {
-      DEBUG_PRINTLN("Package Received on right radio -> RSSI: ");
-      DEBUG_PRINTLN(rssi);
-    } else {
-      DEBUG_PRINTLN("External package on right radio :");
-      DEBUG_PRINT("\tReceived ");
-      DEBUG_PRINT(value);
-      DEBUG_PRINT(" / ");
-      DEBUG_PRINT("Protocol: ");
-      DEBUG_PRINTLN(protocol);
-      DEBUG_PRINT("\tRSSI: ");
-      DEBUG_PRINTLN(rssi);
-    }
-
-    rswitch.resetAvailable();
-
-    return rssi;
-  } else {
-    return 0;
-  }
-}
-
 
 int GetSignalLeft() {
   if (lswitch.available()) {
     int value = lswitch.getReceivedValue();
     int protocol = lswitch.getReceivedProtocol();
-    int rssi = analogRead(1);
+    int rssi = analogRead(0);
     if (value == 47869 && protocol == 5) {
       DEBUG_PRINT("Package Received on left radio -> RSSI: ");
       DEBUG_PRINTLN(rssi);
@@ -59,6 +31,33 @@ int GetSignalLeft() {
     }
 
     lswitch.resetAvailable();
+
+    return rssi;
+  } else {
+    return 0;
+  }
+}
+
+int GetSignalRight() {
+  if (rswitch.available()) {
+    int value = rswitch.getReceivedValue();
+    int protocol = rswitch.getReceivedProtocol();
+    int rssi = analogRead(1);
+    if (value == 47869 && protocol == 5) {
+      DEBUG_PRINTLN("Package Received on right radio -> RSSI: ");
+      DEBUG_PRINTLN(rssi);
+    } else {
+      DEBUG_PRINTLN("External package on right radio :");
+      DEBUG_PRINT("\tReceived ");
+      DEBUG_PRINT(value);
+      DEBUG_PRINT(" / ");
+      DEBUG_PRINT("Protocol: ");
+      DEBUG_PRINTLN(protocol);
+      DEBUG_PRINT("\tRSSI: ");
+      DEBUG_PRINTLN(rssi);
+    }
+
+    rswitch.resetAvailable();
 
     return rssi;
   } else {
