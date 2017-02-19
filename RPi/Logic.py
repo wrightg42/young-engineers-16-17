249eeac7c7395e91582e02153916875f7d2868b3
George Wright <pi@raspberrypi.(none)>
2017-02-19 02:39:25 +0000

    Add support to prase GPS data sent to RPi

--- RPi/Logic.py
+++ RPi/Logic.py
@@ -1,5 +1,6 @@
 import GUI
 import Coms
+import struct
 
 commands = [["forward", "w", "↑"], ["backward", "s", "↓"], ["left", "a", "←"], ["right", "d", "→"]]
 keys_down = []
@@ -22,8 +23,13 @@ def scan():
     Coms.send(15)
     val = Coms.read()
 
-    if val[0] == 17:
+    if val != None and val[0] == 17:
         print("Recieved data: ", val)
+        Coms.send(10) # Get gps location of bot
+        gps = Coms.read()
+        if gps != None and gps[0] == 12:
+            gps = parse_gps(gps)
+            print(gps)
 
 def handle_key(key, pressed = True):
     key = key.lower() # Convert to lower case incase shift was held during key press
@@ -38,4 +44,13 @@ def handle_key(key, pressed = True):
         # change the movement if key was released
         keys_down.remove(key)
         move_change(keys_down[-1] if len(keys_down) != 0 else 0)
-         
+
+def parse_gps(data):
+    gps_data = []
+    gps_data.append(data[1]) # fix
+    gps_data.append(data[2]) # fix quality
+    gps_data.append(struct.unpack("<f", struct.pack("4B", *data[3:7]))) # latitude
+    gps_data.append(struct.unpack("<f", struct.pack("4B", *data[7:11]))) # longitude
+    gps_data.append(data[11]) # altitude
+    gps_data.append(data[12]) # satellites
+    return gps_data
