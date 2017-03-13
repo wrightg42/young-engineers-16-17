5fa6a7ef07db4053da02e712790418fdc28cf4d4
George Wright <wrightgeorge42@gmail.com>
2017-03-13 21:39:40 +0000

    Add security to data payload from Arduino to Pi

--- RPi/Coms.py
+++ RPi/Coms.py
@@ -34,17 +34,21 @@ def send(msg):
     print(msg)
     radio.write(msg)
 
-def read():
+def read(timeout = 1000):
+    corrupt = False
     msg = []
     radio.startListening()
     radio.openReadingPipe(1, pipes[1])
 
     start = datetime.datetime.now()
-    while (datetime.datetime.now() - start).seconds < 1:
+    while (datetime.datetime.now() - start).total_seconds() * 1000 < timeout:
         while radio.available():
             data = []
             radio.read(data, radio.getDynamicPayloadSize())
-            msg.append(bytesToInt(data))
+            if corrupted_bytes(data):
+                currupt = True
+            data = fix_bytes(data)
+            msg.append(bytes_to_int(data))
         if msg != []:
             break
 
@@ -53,15 +57,43 @@ def read():
     radio.openWritingPipe(pipes[0])
     radio.powerDown()
     radio.powerUp()
-    if msg == []:
+    if corrupt:
+        print("Corrupt payload")
+        return "CORRUPT"
+    elif msg == []:
         print("Timed Out")
         return None
     else:
         return msg
-   
-def bytesToInt(bytes):
+
+def corrupted_bytes(bytes):
+    # Check each byte hasn't been corrupted
+    for byte in bytes:
+        binary = bin(byte)[2:]
+        if len(binary) % 2 == 1:
+            binary = "0" + binary
+        for i in range(int(len(binary) / 2)):
+            if binary[2 * i] == binary[2 * i + 1]:
+                return True
+    
+    return False
+
+def fix_bytes(bytes):
+    new_bytes = []
+
+    # Fix each byte tp remove the security from corruption
+    for byte in bytes:
+        binary = bin(byte)[2:]
+        if len(binary) % 2 == 1:
+            binary = "0" + binary
+        new_byte = binary[::2]
+        new_bytes.append(int(new_byte, 2))
+
+    return new_bytes
+
+def bytes_to_int(bytes):
     i = 0
     for j in range(len(bytes) - 1, -1, -1):
-        i += bytes[j] << (8 * j)
+        i += bytes[j] << (4 * j)
 
     return i
