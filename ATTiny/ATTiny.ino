c2e540b1f80b3842bc2bb14b34fc8b7df5e2a7cd
Luke Ashford <luke.a.ashford@gmail.com>
2017-03-16 17:14:01 +0000

    Transmit signal constantly
    Previously the signal only transmitted four times each second. This change will transmit 20 times per second in order to improve the reliability of scanning.

--- ATTiny/ATTiny.ino
+++ ATTiny/ATTiny.ino
@@ -2,12 +2,13 @@
 RCSwitch mySwitch = RCSwitch();
  
 void setup() {
-  mySwitch.enableTransmit(6); // pin 6 on arduino uno. change to 0 for attiny85
+  // Transmitter is connected to physical pin 5, software pin 0
+  mySwitch.enableTransmit(0);
   mySwitch.setProtocol(5);
   mySwitch.setRepeatTransmit(15);
 }
  
 void loop() {
   mySwitch.send(47869, 16);
-  delay(250);
+  delay(5);
 }
