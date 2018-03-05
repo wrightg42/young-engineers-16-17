bc1b225cb742da219938bce0de63ba9e882656b5
George Wright <wrightgeorge42@gmail.com>
2018-03-05 14:08:57 +0000

    Add optional GPS argument on execution

--- RPi/main.py
+++ RPi/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import gui
 import logic
 import coms
@@ -7,4 +9,7 @@ def main():
     gui.init_window()
     
 if __name__ == "__main__":
+    arguments = [s.lower() for s in sys.argv[1:]]
+    if "gps" in arguments or "-gps" in arguments:
+        logic.get_gps = True
     main()
